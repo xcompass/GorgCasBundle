1b884bd05965c470d7e5bb84b2afbc6ae3b08a57
Pan Luo <pan.luo@ubc.ca>
2016-04-24 00:48:08 -0700

    Make it compatible with Symfony 2.8

--- Security/Firewall/CasListener.php
+++ Security/Firewall/CasListener.php
@@ -20,16 +20,8 @@
  ***************************************************************************/
 namespace Gorg\Bundle\CasBundle\Security\Firewall;
 
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Log\LoggerInterface;
-use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
-use Symfony\Component\Security\Core\SecurityContextInterface;
-use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
-use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 use Symfony\Component\Security\Http\Firewall\AbstractAuthenticationListener;
-use Symfony\Component\Security\Http\HttpUtils;
-use Symfony\Component\Security\Http\Session\SessionAuthenticationStrategyInterface;
 use Symfony\Component\Security\Core\Role\Role;
 
 /**
@@ -42,14 +34,6 @@ use Symfony\Component\Security\Core\Role\Role;
  */
 class CasListener extends AbstractAuthenticationListener
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function __construct(SecurityContextInterface $securityContext, AuthenticationManagerInterface $authenticationManager, SessionAuthenticationStrategyInterface $sessionStrategy, HttpUtils $httpUtils, $providerKey, AuthenticationSuccessHandlerInterface $successHandler, AuthenticationFailureHandlerInterface $failureHandler, array $options = array(), LoggerInterface $logger = null, EventDispatcherInterface $dispatcher = null)
-    {
-        parent::__construct($securityContext, $authenticationManager, $sessionStrategy, $httpUtils, $providerKey, $successHandler, $failureHandler, $options, $logger, $dispatcher);
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -74,7 +58,7 @@ class CasListener extends AbstractAuthenticationListener
             $attributes = \phpCAS::getAttributes();
 
             // username
-            if (in_array('username', $mapping) && in_array($mapping['username'], $attributes)) {
+            if (in_array('username', $mapping, true) && in_array($mapping['username'], $attributes, true)) {
                 $user = $attributes[$mapping['username']];
             }
             // roles
